4b0e6ba1f7b55150b4b9c80c0eb1a478d734928f
ZakharTsarapov <tsarapovs@gmail.com>
2023-03-12 09:17:09 +0200

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const onContainerGallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+onContainerGallery.insertAdjacentHTML(
+  'beforeend',
+  createGalleryMarkup(galleryItems)
+);
+
+let slider = new SimpleLightbox('.gallery .gallery__item', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
