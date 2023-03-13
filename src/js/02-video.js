14da80f45fa81180073aa018ced533ea4672e286
ZakharTsarapov <tsarapovs@gmail.com>
2023-03-13 19:49:19 +0200

    hope done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const time = data.seconds;
+    localStorage.setItem(STORAGE_KEY, time);
+  }, 1000)
+);
+
+const playedTime = localStorage.getItem(STORAGE_KEY);
+if (playedTime) {
+  player.setCurrentTime(playedTime);
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {}
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+onSavedSymbols();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formRefs = event.currentTarget.elements;
+  const email = formRefs.email.value;
+  const textarea = formRefs.message.value;
+
+  if (email.trim() === '' || textarea === '') {
+    return alert(
+      'Внимание, Внимание !!! Все поля должны быть заполнены чертов Мазафака!!!'
+    );
+  }
+    formData = {};
+    
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSavedSymbols() {
+  const savedSymbols = localStorage.getItem(STORAGE_KEY);
+    if (savedSymbols) {
+        formData = JSON.parse(savedSymbols);
+    };
+    const entries = Object.entries(formData);
+
+    entries.forEach(function (el) {
+      const [key, value] = el;
+      refs.form[key].value = value;
+    });
+}
